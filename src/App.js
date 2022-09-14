df4b4356f8e6222260f51375a1deb6925cf8145d
Phaedon Valasiadis <faival@protonmail.com>
2022-09-14 15:55:49 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      foodstats: []
+    };
+  }
+
+  getFoodStats() {
+    var newIngredient = document.getElementById('ing').value;
+    console.log(newIngredient);
+
+     //Use your app's API Key here:
+    var api_key = 'DEMO_KEY'; 
+    var base_url = `https://api.nal.usda.gov/fdc/v1/foods/search?query=${newIngredient}&pageSize=2&api_key=`;
+    base_url+=api_key;
+    base_url+='&q=';
+    base_url+=newIngredient;
+
+    fetch(base_url)
+     .then((response) => {
+       return response.json();
+     })
+     .then((data) => {
+       this.setState({
+         foodstats: data.foodstats
+     });
+     console.log(data);
+     })
+  }  
+
+  render() {
+    return (
+      <div className="App">
+        <h4> Enter an ingredient </h4>
+        <div>
+          <input type="text" name="ingredient" id="ing"/>
+        </div>
+        <div>
+          <button onClick={() => this.getFoodStats()}>Add Ingredient</button>
+        </div>
+      </div>
+    );
+}
 }
 
 export default App;
